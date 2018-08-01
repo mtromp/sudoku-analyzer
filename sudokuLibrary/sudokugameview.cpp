e3d24841e2bc6c028569fce4399ce50da40c8fe0
Marianne Tromp <mtromp@mtromp.com>
2018-07-31 20:47:17 -0400

    Set some view attributes

--- sudokuLibrary/sudokugameview.cpp
+++ sudokuLibrary/sudokugameview.cpp
@@ -2,5 +2,9 @@
 
 SudokuGameView::SudokuGameView(QWidget *parent) : QGraphicsView(parent)
 {
+    this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    this->setFrameStyle(QFrame::NoFrame);
 
+    this->setBackgroundBrush(QColor(114, 273, 175));
 }
