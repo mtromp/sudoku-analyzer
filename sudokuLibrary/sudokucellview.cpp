f1777a36facc430a635b2ea367fd6780bcf86b31
Marianne Tromp <mtromp@mtromp.com>
2019-07-07 19:16:32 -0400

    Print the selected number in the center of the cell

--- sudokuLibrary/sudokucellview.cpp
+++ sudokuLibrary/sudokucellview.cpp
@@ -21,41 +21,62 @@ QRectF SudokuCellView::boundingRect() const
 }
 
 void SudokuCellView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
-           QWidget *widget)
+                           QWidget *widget)
 {
     Q_UNUSED(widget);
     Q_UNUSED(option);
 
-    painter->setPen(QPen(QColor(56, 165, 211), 1));
-    painter->drawRect(0, 0, Dimensions::cellSize, Dimensions::cellSize);
+    if (this->theValue == 0) {
+        painter->setPen(QPen(QColor(56, 165, 211), 1));
+        painter->drawRect(0, 0, Dimensions::cellSize, Dimensions::cellSize);
 
-    QFont font = QFont(painter->font());
-    font.setPixelSize(Dimensions::cellChoiceFontSize);
-    painter->setFont(font);
+        QFont font = QFont(painter->font());
+        font.setPointSize(Dimensions::cellChoiceFontSize);
+        painter->setFont(font);
 
-    int spacing = (Dimensions::cellSize - 3 * Dimensions::cellChoiceFontSize) / 4;
-    int xoffset = spacing;
-    int yoffset = spacing;
+        int spacing = (Dimensions::cellSize - 3 * Dimensions::cellChoiceFontSize) / 4;
+        int xoffset = spacing;
+        int yoffset = spacing;
 
-    for(int number = 1; number <= 9; number++)
-    {
-        painter->setPen(QPen(QColor(3,73,104)));
+        for(int number = 1; number <= 9; number++)
+        {
+            painter->setPen(QPen(QColor(3,73,104)));
+
+            painter->drawText(QRectF(
+                                  xoffset,
+                                  yoffset,
+                                  Dimensions::cellChoiceFontSize,
+                                  Dimensions::cellChoiceFontSize),
+                              Qt::AlignCenter | Qt::AlignVCenter,
+                              QString::number(number)
+                              );
+            if ( number == 3 || number == 6)
+            {
+                yoffset = yoffset + Dimensions::cellChoiceFontSize + spacing;
+                xoffset = 2;
+            } else {
+                xoffset = xoffset + Dimensions::cellChoiceFontSize + spacing;
+            }
+        }
+    }
+    else {
+        painter->setPen(QPen(QColor(56, 165, 211), 1));
+        painter->drawRect(0, 0, Dimensions::cellSize, Dimensions::cellSize);
+
+        QFont font = QFont(painter->font());
+        font.setPointSize(20);
+        painter->setFont(font);
+        painter->setPen(QPen(QColor(203,73,104)));
 
         painter->drawText(QRectF(
-                                 xoffset,
-                                 yoffset,
-                                 Dimensions::cellChoiceFontSize,
-                                 Dimensions::cellChoiceFontSize),
+                              0,
+                              0,
+                              Dimensions::cellSize,
+                              Dimensions::cellSize),
                           Qt::AlignCenter | Qt::AlignVCenter,
-                          QString::number(number)
-                    );
-        if ( number == 3 || number == 6)
-        {
-            yoffset = yoffset + Dimensions::cellChoiceFontSize + spacing;
-            xoffset = 2;
-        } else {
-            xoffset = xoffset + Dimensions::cellChoiceFontSize + spacing;
-        }
+                          QString::number(this->theValue)
+                          );
+
     }
 }
 void SudokuCellView::handleMouseEvent()
